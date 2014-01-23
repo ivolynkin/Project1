810f26ba1f68c7285c423aa22fa0842ee0450918
Ivan Volynkin <ivan_volynkin@student.uml.edu>
2014-01-23 13:40:58 -0500

    added one more shit

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -16,6 +16,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!!!!");
-        System.out.printls("HUJ VAM VSEM");
+        
+        System.out.println("HUJ VAM VSEM");
+        System.out.println("HUJ VAM VSEM URA");
+        
     }
 }
